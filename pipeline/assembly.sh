044a164463c95135ef94ad7ec683aa97885a61d8
sbrown8 <sbrow84@uwo.ca>
2016-03-13 12:22:27 +0000

    Added assembly and partial annotation pipeline

--- pipeline/assembly.sh
+++ pipeline/assembly.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 readFolder=$1;
 
 forward="";
-for f in $readFolder/*
+for f in $readFolder/*.fastq
 do
   baseFileName=${f##*/};
   if [[ $forward == "" ]]; then
@@ -11,7 +11,8 @@ do
   else
     sampleName=${baseFileName%%_*};
     echo "running $forward and $f for $sampleName";
-    Trinity --seqType fq --max_memory 200G --left $forward --right $f --CPU 12 --full_cleanup --output $readFolder/trinity_$sampleName > $readFolder/trinity_$sampleName/$sampleName"_assembly.log";
+    mkdir $readFolder/trinity_$sampleName;
+    Trinity --seqType fq --max_memory 50G --left $forward --right $f --CPU 12 --full_cleanup --output $readFolder/trinity_$sampleName > $readFolder/trinity_$sampleName/$sampleName"_assembly.log";
     forward="";
   fi
 done
