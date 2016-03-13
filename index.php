45c91eeec737d1a52cd6ff46060071870162c6c4
Shannon Brown <sbrown8@outlook.com>
2016-03-13 04:16:52 -0400

    bootstrap css separate

--- index.php
+++ index.php
@@ -10,10 +10,7 @@ and open the template in the editor.
         <title>BioHack Project</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 			<!-- Latest compiled and minified CSS -->
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
-			<!-- Optional theme -->
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+			<link rel="stylesheet" src="css/bootstrap.min.css">
 
 			<!-- Latest compiled and minified JavaScript -->
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
